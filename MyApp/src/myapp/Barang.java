b5d77772639c5f332e111e759858d66c0fe35bd4
Chevyosa <riyandaazis00@gmail.com>
2023-06-04 19:46:44 +0700

    Added New Files
    Added Main Program:
    - MyApp
    
    Added New Form
    - LoginForm
    - FormPayment

---
+++ MyApp/src/myapp/Barang.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myapp;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Barang {
+    public String kode;
+    public String nama;
+    public float harga;
+}
